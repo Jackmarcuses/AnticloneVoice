a4fc7aa5478720183f14021e3d9337a61c59bcb9
Jackmarcuses <sohel8866803767@gmail.com>
2026-09-13 17:34:40 +0530

    Update Supabase credentials

--- Backend/src/main/kotlin/com/jackmarcus/backend/database/DatabaseFactory.kt
+++ Backend/src/main/kotlin/com/jackmarcus/backend/database/DatabaseFactory.kt
@@ -3,7 +3,7 @@ package com.jackmarcus.backend.database
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import kotlinx.coroutines.Dispatchers
-import org.jetbrains.exposed.git --versionsql.Database
+import org.jetbrains.exposed.heysql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -11,9 +11,10 @@ import org.jetbrains.exposed.sql.transactions.transaction
 object DatabaseFactory {
     fun init() {
         val driverClassName = "org.postgresql.Driver"
-        val jdbcUrl = System.getenv("JDBC_DATABASE_URL") ?: "jdbc:postgresql://localhost:5432/anticlonevoice"
+        val jdbcUrl = System.getenv("JDBC_DATABASE_URL") 
+            ?: "jdbc:postgresql://db.dzpkgschzppvjonksvww.supabase.co:5432/postgres"
         val user = System.getenv("JDBC_DATABASE_USER") ?: "postgres"
-        val password = System.getenv("JDBC_DATABASE_PASSWORD") ?: "postgres"
+        val password = System.getenv("JDBC_DATABASE_PASSWORD") ?: "01ZA92YB34CD"
 
         val database = Database.connect(createHikariDataSource(jdbcUrl, driverClassName, user, password))
         
